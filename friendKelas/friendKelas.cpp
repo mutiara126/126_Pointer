7bfb407dc86275346dddf50df6893d7c4e0ee208
mutiara126 <salsamutiara1812@gmail.com>
2023-05-08 12:51:05 +0800

    menambahkan int main ke friend

--- friendKelas/friendKelas.cpp
+++ friendKelas/friendKelas.cpp
@@ -4,14 +4,36 @@ using namespace std;
 
 class siswa;
 
-class orang{
+class orang {
 private:
 	   string nama;
 public:
 	void setNama(string pNama);
 	friend class siswa;
 };
+
 class siswa {
 private:
 	int id;
+public:
+	void setId(int pId);
+	void displayAll(orang& a);
 };
+
+void siswa::displayAll(orang& a){
+	cout << id << endl << a.nama;
+}
+void orang :: setNama(string pNama) {
+	nama = pNama;
+}
+void siswa::setId(int pId) {
+	id = pId;
+}
+int main() {
+	orang joko;
+	joko.setNama("Joko Kumat");
+	siswa joko_siswa;
+	joko_siswa.setId(1);
+	joko_siswa.displayAll(joko);
+	return 0;
+}
